bc6fe3016bd4f929a483ce443ed8426aad2b2db4
Emir Kokum <emir_kokum_@hotmail.com>
2025-05-24 01:44:54 +0300

    İnitial Commit For Frontend

--- CegCRMAPI/CegCRMClient/src/main.tsx
+++ CegCRMAPI/CegCRMClient/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
+import "./index.css";
+import router from "./app/router.tsx";
+import { store } from "./app/store.ts";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
+);
